9605d08cc08d937fd15a6fb357b1a66ca4b7bba3
nkdd <dinuhai@yahoo.co.in>
2020-06-01 15:27:46 +0530

    add platform

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+//var platform;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -36,6 +37,7 @@ function setup(){
 
     bird = new Bird(100,100);
 
+    //platform = new Ground(150,305,300,170);
 }
 
 function draw(){
@@ -58,6 +60,7 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+
 
     bird.display();
     platform.display();
